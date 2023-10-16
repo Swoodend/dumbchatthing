fa8a75953f33ab8268be6dc52b8e99e279573cb5
swoodend <scott.woodend@zapier.com>
2023-10-15 22:49:34 -0400

    add client side socket listener for chat messages

--- src/components/ChatReadout/ChatReadout.tsx
+++ src/components/ChatReadout/ChatReadout.tsx
@@ -1,8 +1,23 @@
+import { socketEvents } from '../../backend/socket_server/events';
+import React from 'react';
+import { socket } from '../../socket';
 import './styles.css';
 const ChatReadout = () => {
+  const [messages, setMessages] = React.useState<string[]>([]);
+
+  React.useEffect(() => {
+    socket.on(socketEvents.CLIENT_MESSAGE, (message) => {
+      setMessages([...messages, message]);
+    });
+  }, []);
+
   return (
     <div className="chat-readout-container">
-      <div className="chat-readout"></div>
+      <div className="chat-readout">
+        {messages.map((msg) => {
+          <p style={{ padding: '10px 0px' }}>{msg}</p>;
+        })}
+      </div>
     </div>
   );
 };
